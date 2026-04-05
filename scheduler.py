13ab63e77c4f1d8ac1786dc816e8e06574ad14cb
Jesse Eliot <jesse.eliot@student.manchester.ac.uk>
2026-04-05 17:23:12 +0100

    feat: scheduler script orchestrates running searches and sending results to users

---
+++ scheduler.py
@@ -0,0 +1,16 @@
+from db import get_searches_due, get_subscribed_users, update_last_run
+from tracker import run_search
+from messaging import send_results_to_user
+
+def handler(event, context):
+    searches = get_searches_due()
+
+    for search in searches:
+        results = run_search(search)
+
+        users = get_subscribed_users(search["id"])
+
+        for user in users:
+            send_results_to_user(user, results)
+
+        update_last_run(search["id"])
