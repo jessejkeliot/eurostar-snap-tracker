57444c11736fb030dc15be57b905a865bb3cbc14
Jesse Eliot <jesse.eliot@student.manchester.ac.uk>
2026-04-07 14:55:34 +0100

    fix: change to lighter bottle api

--- handler.py
+++ handler.py
@@ -1,11 +1,16 @@
 from services import add_subscription, should_run_now
-from db import get_search_by_id
+from db import get_search_by_id, get_user_by_phone_number
 from tracker import run_search
-from messaging import send_results_to_user
+# from messaging import send_results_to_user
+import bottle
 
-def handler(event, context):
-    user_id = extract_user(event)
-    params = parse_message(event)
+@bottle.route("/", method="POST")
+def handler():
+    phone_number = bottle.request.form.get("From")
+    message = bottle.request.form.get("Body")
+    
+    user_id = get_user_by_phone_number(phone_number) # table search
+    params = parse_message(message) # use gemma
 
     search_id, is_new = add_subscription(user_id, **params)
 
@@ -14,3 +19,13 @@ def handler(event, context):
     if is_new or should_run_now(search):
         results = run_search(search)
         send_results_to_user(user_id, results)
+    return "OK"
+
+def parse_message(message):
+    # make gemma 4 call with our system prompt
+    
+    
+    return {"origin": "Unknown",
+    "destination": "Unknown",
+    "outbound_date": "Unknown",
+    "inbound_date": "Unknown"}
