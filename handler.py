107dd5980150c6f71eee6dc8edd4515a6d8105a2
Jesse Eliot <jesse.eliot@student.manchester.ac.uk>
2026-04-08 09:45:17 +0100

    fix: use ai output in handler

--- handler.py
+++ handler.py
@@ -2,7 +2,7 @@ from models import MinimalSearch
 from services import add_subscription, should_run_now
 from db import get_search_by_id, get_user_by_phone_number
 from tracker import run_search
-from messaging import send_results_to_user
+from messaging import parse_message, send_results_to_user
 # from messaging import send_results_to_user
 import bottle
 
@@ -18,26 +18,20 @@ def handler():
         # perhaps create user or something, but for now
         user_id = None
     params = parse_message(message) # use gemma
+    if (params):
+        search_id, is_new = add_subscription(user_id, params.origin, params.destination, params.outbound_date, params.inbound_date)
 
-    search_id, is_new = add_subscription(user_id, **params)
+        search = get_search_by_id(search_id)
 
-    search = get_search_by_id(search_id)
-
-    if search and (is_new or should_run_now(search)):
-        ms = MinimalSearch(search.origin, search.destination, search.outbound_date, search.inbound_date)
-        results = run_search(ms)
-        send_results_to_user(user_id, results)
-    return "OK"
+        if search and (is_new or should_run_now(search)):
+            ms = MinimalSearch(search.origin, search.destination, search.outbound_date, search.inbound_date)
+            results = run_search(ms)
+            send_results_to_user(user_id, results)
+        return "OK"
+    else:
+        # TODO send a message asking for user to repeat themselves
+        pass
 
 @bottle.route("/webhook/email", method="POST")
 def email_handler():
     email = bottle.request.form.get
-
-def parse_message(message):
-    # make gemma 4 call with our system prompt
-    
-    
-    return {"origin": "Unknown",
-    "destination": "Unknown",
-    "outbound_date": "Unknown",
-    "inbound_date": "Unknown"}
