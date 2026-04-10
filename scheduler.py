1d6dcda28a126c2d033c6b3c9b7b1db2ff18798e
Jesse Eliot <jesse.eliot@student.manchester.ac.uk>
2026-04-11 00:14:07 +0100

    refactor: scheduler

--- scheduler.py
+++ scheduler.py
@@ -4,33 +4,36 @@ from tracker import TrainJourney, run_search
 from messaging import send_results_to_user
 from datetime import datetime, timedelta
 def handler():
-    searches = get_searches_due()
-    handle_search(searches)
-
-def handle_search(searches: list[Search]):
-    # filter the searches
-    today = datetime.now().date()
-    filtered = [search for search in searches if (search.outbound_date - timedelta(days=15) < today)]
-    for search in filtered:
-        # create a minimal search object that only contains the data needed for constructing a search url
-        search_and_send(search)
+    active_searches = get_active_searches()
+    
+    for search in active_searches:
+        results, results_string, has_changed = check_for_search_updates(search)
         
-def search_and_send(search : Search):
+        if has_changed:
+            users = get_subscribed_users(search.id)
+            for user in users:
+                send_results_to_user(user.id, results)
+                
+            update_last_run(search.id, results_string)
+        else:
+            update_last_checked(search.id)
+            print(f"On latest run of search (id: {search.id}) the results have not changed")
+
+def get_active_searches() -> list[Search]:
+    """Retrieves searches that are due and within the 15-day active window."""
+    searches = get_searches_due()
+    today = datetime.now().date()
+    return [search for search in searches if (search.outbound_date - timedelta(days=15) < today)]
+
+def check_for_search_updates(search: Search) -> tuple[list[TrainJourney], str, bool]:
+    """Executes the search and checks if the results differ from the last run."""
+    # create a minimal search object that only contains the data needed for constructing a search url
     ms = MinimalSearch(search.origin, search.destination, search.outbound_date, search.inbound_date)
     url, results = run_search(ms)
-    result_joined = " ".join([str(tj) for tj in results])
-    hd = hash_for_db(result_joined)
-    # if the last results are equal to the new ones ( Both are hashed ) then just log it
-    if(search.last_results != hd):
-        send_to_subscribed_users(search.id, results)
-        update_last_run(search.id, result_joined)   
-    else:
-        update_last_checked(search.id)
-        print(f"On lastest run of search (id: {search.id}) the results have not changed")
-
     
-def send_to_subscribed_users(search_id, results: list[TrainJourney]):
-    users = get_subscribed_users(search_id)
-
-    for user in users:
-        send_results_to_user(user.id, results)
+    results_string = " ".join([str(tj) for tj in results])
+    hashed_results = hash_for_db(results_string)
+    
+    has_changed = (search.last_results != hashed_results)
+    
+    return results, results_string, has_changed
